697e738229d368c5aca60189cd4816eb4477f507
Amani <Amanikhunter@gmail.com>
2019-12-09 08:47:20 -0500

    Initial Commit

---
+++ FileExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FileExample
+//
+//  Created by Amani Hunter on 12/9/19.
+//  Copyright © 2019 Amani Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

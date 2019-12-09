04f83b9ad16a88826ff2460c2f0413e1430800c2
Amani <Amanikhunter@gmail.com>
2019-12-09 10:04:20 -0500

    Saving and reading files.

--- FileExample/ViewController.swift
+++ FileExample/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textBox: UITextField!
+    
+    var fileMgr: FileManager = FileManager.default
+    var docsDir: String?
+    var dataFile: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        view.addGestureRecognizer(tap)
+        
+        let filemgr = FileManager.default
+        
+        let dirPaths = filemgr.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        dataFile = dirPaths[0].appendingPathComponent("datafile.dat").path
+        
+        if fileMgr.fileExists(atPath: dataFile!){
+            let databuffer = fileMgr.contents(atPath: dataFile!)
+            let datastring = NSString(data: databuffer!, encoding: String.Encoding.utf8.rawValue)
+            
+            textBox.text = datastring as? String
+        }
     }
 
-
+    @IBAction func saveText(_ sender: Any) {
+        let databuffer = (textBox.text)!.data(using: String.Encoding.utf8)
+        
+        fileMgr.createFile(atPath: dataFile!, contents: databuffer, attributes: nil)
+        
+    }
+    
 }
 
